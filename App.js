7632c8c040f804005a76621892f1a9c7a45a7a2e
Santiago Alurralde <santiago.alurralde@gmail.com>
2019-04-26 14:43:59 +1000

    Last changes

--- App.js
+++ App.js
@@ -1,22 +1,75 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, Text, View } from 'react-native';
 
 import FetchLocation from './components/FetchLocation';
 import UsersMap from './components/UsersMap';
 
 export default class App extends React.Component {
-  getUserLocationHandler() {
-    console.log('Y Eia');
-    navigator.geolocation.getCurrentPosition(position => {
-      console.log('Position ', position);
-    }, err => console.log('Error!', err))
+  constructor(props) {
+    super(props);
+    this.state = {
+      userLocation: null,
+      usersPlaces: []
+    }
+    this.getUserLocationHandler = this.getUserLocationHandler.bind(this);
+    this.getUsersPlacesHandler = this.getUsersPlacesHandler.bind(this);
   }
+
+  getUserLocationHandler = () => {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          userLocation: {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            latitudeDelta: 0.0622,
+            longitudeDelta: 0.0421
+          }
+        });
+        fetch("https://pwaguide-f8cf3.firebaseio.com/places.json", {
+          method: "POST",
+          body: JSON.stringify({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+          })
+        })
+          .then(res => console.log(res))
+          .catch(err => console.log(err));
+      },
+      err => console.log(err)
+    );
+  };
+
+  getUsersPlacesHandler = () => {
+    fetch("https://pwaguide-f8cf3.firebaseio.com/places.json")
+      .then(res => res.json())
+      .then(parsedRes => {
+        const placesArray = [];
+        for (const key in parsedRes) {
+          placesArray.push({
+            latitude: parsedRes[key].latitude,
+            longitude: parsedRes[key].longitude,
+            id: key
+          });
+        }
+        this.setState({
+          usersPlaces: placesArray
+        });
+      })
+      .catch(err => console.log(err));
+  };
 
   render() {
     return (
       <View style={styles.container}>
+        <View style={{ marginBottom: 20 }}>
+          <Button title="Get User Places" onPress={this.getUsersPlacesHandler} />
+        </View>
         <FetchLocation onGetLocation={this.getUserLocationHandler} />
-        <UsersMap />
+        <UsersMap
+          userLocation={this.state.userLocation}
+          usersPlaces={this.state.usersPlaces}
+        />
       </View>
     );
   }
